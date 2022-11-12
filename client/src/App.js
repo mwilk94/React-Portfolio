c7fe31f35b51c20cfac5440e4b2b6e794cca6d0c
Merkisha Wilkinson <merkisha94@gmail.com>
2022-11-12 00:27:02 -0500

    added info in app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,20 @@
 import "./App.css";
 import Navbar from "./components/Navbar";
 import Home from "./components/Home";
+import About from "./components/About";
+import Portfolio from "./components/Skills";
+import Resume from "./components/Work";
+import Contact from "./components/Contact";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
       <Home />
+      <About />
+      <Portfolio />
+      <Resume />
+      <Contact />
     </div>
   );
 }
