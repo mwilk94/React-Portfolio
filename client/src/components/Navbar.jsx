6e54584512cdf0c9c67a0926d5f75186ee0ba594
Merkisha Wilkinson <merkisha94@gmail.com>
2022-11-12 01:51:10 -0500

    i dont understand the issue with tailwind?

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -13,9 +13,9 @@ const Navbar = () => {
   
   return (
     <div className='fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0a192f] text-gray-300'>
-      {/* <div>
+      <div>
         <img src={Logo} alt='Logo' style={{ width: '80px' }} />
-      </div> */}
+      </div>
 
       {/* menu */}
       <ul className='hidden md:flex'>
@@ -47,7 +47,7 @@ const Navbar = () => {
       </ul>
 
       {/* Hamburger */}
-      <div onClick={handleClick} className='md:hidden z-10'>
+      <div onClick={handleClick} className=' z-10'>
         {!nav ? <FaBars /> : <FaTimes />}
       </div>
 
